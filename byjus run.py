63761d6f63ca070449e1bb4b86a1a963006e2ec6
sanatnkulkarni <75944828+sanatnkulkarni@users.noreply.github.com>
2020-12-22 19:46:19 +0400

    Add files via upload

---
+++ byjus run.py
@@ -0,0 +1,7 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==== RESTART: C:/Users/Sanat/AppData/Local/Programs/Python/Python38/byjus.py ===
+welcome to byjus the learning app
+this app clears all your doubts in science and maths and later history and civics
+username: 
